ba8cbe35b6a77feb575659032a37a69f01b1df33
shuvarek <shuvarek@gmail.com>
2015-03-19 14:32:21 +0000

    some cleaning

--- twelve.js
+++ twelve.js
@@ -7,8 +7,12 @@ var poskeyx = [1,1,2,3,3];			// positions of the keys representing the group of 
 var poskeyy = [1,3,2,1,3];			// ... and y
 
 // ...and variables
-var movpcsx = [0, 0, 0, 0];		// temporary array of pieces which are moving (from positions in the movof array )
 
+// array of pieces - which number is in any (+96 gives a letter)
+var piece = [0,1,2,3,4,5,6,7,8,9,10,11];
+var pieceoffset = [0,1,3,5,6];	// offset of turning pieces depends on which key is pressed 
+											// not used as it is not made in loop now (in movepieces)
+											
 // array of the pieces (in which column and row they are or the table)
 var posx = new Array(nop);
 var posy = new Array(nop);
@@ -19,9 +23,10 @@ for (var i = 0; i<nop; i++ ) {
 	posy[i] = (n - rem) / dim;
 }
 
-//
+
+//*************
 // this values are dependable of the rendered page.
-//
+//*************
 
 var maintitle = document.getElementById("maintitle");
 var myfooter = document.getElementById("myfooter");
@@ -36,45 +41,66 @@ for ( i1 = 0; i1 < dim; i1++) {
 }
 
 
-//
+//*************
 // funcions
-//
+//*************
 
 function refreshpieces() {
 	for (var i = 0; i < nop; i++ ) {
-		tcs[posx[i]][posy[i]].innerHTML = String.fromCharCode(i+97);
+		tcs[posx[i]][posy[i]].innerHTML = String.fromCharCode(piece[i]+97);
 	}
 }
 
 function refreshkeys(colorkeys) {	// i. e. colorkeys = '#cc0'; 
- 	for ( i = 0; i<5; i++) {
+ 	for (var i = 0; i<5; i++) {
  		tcs[poskeyy[i]][poskeyx[i]].innerHTML = i+1;
  		tcs[poskeyy[i]][poskeyx[i]].style.backgroundColor = colorkeys;
  	}
 }
 
-// To be done - moving pieces
+function resetpieces() {
+	for (var i = 0; i<nop; i++) {
+		piece[i] = i;
+	}
+}
+
+// moving pieces - main part of the game
 function movepics(num, direct) {	// num is the key - 1 (so 0 is for the key 1)
 	var whichtcs = tcs[poskeyy[num]][poskeyx[num]];
 	if(direct) {	
 		whichtcs.style.backgroundColor = "#88f";
+		//[0, 3, 5, 2]		
+		var tmp = piece[0+pieceoffset[num]];
+		piece[0+pieceoffset[num]] = piece[3+pieceoffset[num]];
+		piece[3+pieceoffset[num]] = piece[5+pieceoffset[num]];
+		piece[5+pieceoffset[num]] = piece[2+pieceoffset[num]];
+		piece[2+pieceoffset[num]] = tmp;
+			
 	} else {
 		whichtcs.style.backgroundColor = "#8f8";
+		var tmp = piece[0+pieceoffset[num]];
+		piece[0+pieceoffset[num]] = piece[2+pieceoffset[num]];
+		piece[2+pieceoffset[num]] = piece[5+pieceoffset[num]];
+		piece[5+pieceoffset[num]] = piece[3+pieceoffset[num]];
+		piece[3+pieceoffset[num]] = tmp;
+	
 	}
-	myfooter.innerHTML = "shift: " + direct;
+	refreshpieces();
+	whichtcs.style.backgroundColor = "#f88";
 }
 
 function mykeydown(e) {
 	var keycode = e.keyCode;
 	var keychar = String.fromCharCode(keycode);
-	maintitle.innerHTML = "key: " + keychar + " of code: " + keycode;
 	
 	var num = keycode - 49;	// num would be from 0 - 4 (so the key is one value bigger, but this is better for index)
 	if (( num >= 0 && num <= 4))  movepics(num, e.shiftKey);
 }
+
+
 //===========================================================
-
-
+// Main Programm
+//===========================================================
 
 // show pieces for first time
 refreshpieces();
